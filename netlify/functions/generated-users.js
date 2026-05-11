de918c32beb0973e668855395308b3c6c15a1acb
PRO3S <ratchataysr05@gmail.com>
2026-05-11 16:19:36 +0700

    Fix: include generated-users.js for Netlify Function

---
+++ netlify/functions/generated-users.js
@@ -0,0 +1,30 @@
+'use strict';
+exports.users = [
+    {
+        "id":  1,
+        "username":  "ratchata.y@pro3s.com",
+        "password":  "AAaa1234",
+        "role":  "admin",
+        "name":  "System Administrator",
+        "avatar":  "S",
+        "status":  "active"
+    },
+    {
+        "id":  2,
+        "username":  "inspector@company.com",
+        "password":  "Weld#2026",
+        "role":  "inspector",
+        "name":  "Weld Inspector",
+        "avatar":  "W",
+        "status":  "active"
+    },
+    {
+        "id":  3,
+        "username":  "viewer@company.com",
+        "password":  "View#2026",
+        "role":  "viewer",
+        "name":  "Quality Viewer",
+        "avatar":  "Q",
+        "status":  "active"
+    }
+];
